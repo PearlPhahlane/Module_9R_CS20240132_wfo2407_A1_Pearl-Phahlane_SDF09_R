0c5bcd98b020eae64b0039b4c6cc6906262d9e5e
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-30 13:37:30 +0200

    add hasblackjack variable to show you win(true) or lose(false)  and isAlive variable to show if you are still in the game or not (false)

--- index.js
+++ index.js
@@ -4,13 +4,19 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 10
-let secondCard = 12
-let sum = firstCard + secondCard +6 
+let secondCard = 9
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 }  else {
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
+
+console.log(hasBlackJack)
