deb3a34e43a6703d4dd6f14721e6011e85eeee85
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-30 13:45:26 +0200

    add message variable

--- index.js
+++ index.js
@@ -4,18 +4,19 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 10
-let secondCard = 9
+let secondCard = 11
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 }  else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false
 }
 
