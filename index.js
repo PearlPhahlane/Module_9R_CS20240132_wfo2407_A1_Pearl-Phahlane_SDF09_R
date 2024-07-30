1e6bcd7b520ab42691c1d903e1b14885b2bcbe82
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-30 21:39:56 +0200

    invoke start game function and make the start button work

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!";
-    hasBlackJack = true;
-} else if (sum > 21) {
-    message = "You're out of the game!";
-    isAlive = false;
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else if (sum > 21) {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    
+    console.log(message)
+
 }
 
-console.log(message)
