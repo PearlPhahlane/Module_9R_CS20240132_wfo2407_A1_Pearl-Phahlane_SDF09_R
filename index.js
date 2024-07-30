869f349e65a8db5420120dbbb719665850cb3265
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-30 21:26:42 +0200

    add the message variable and remove the console log and brackets

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let firstCard = 10;
-let secondCard= 12;
+let secondCard= 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?");
+    message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!");
+    message = "Wohoo! You've got Blackjack!";
     hasBlackJack = true;
 } else if (sum > 21) {
-    console.log("You're out of the game!")
+    message = "You're out of the game!";
     isAlive = false;
 }
 
-console.log(isAlive)
+console.log(message)
