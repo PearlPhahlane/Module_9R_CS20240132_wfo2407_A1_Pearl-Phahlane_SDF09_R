eec99b03b3e5cd44edd7375ebecb2a15fb0ae3c0
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-30 13:05:02 +0200

    add rule of blackjack game, if sum is exactly 21 a message you win will disaplay, if it is less than 21 it will prompt you to draw new card and if greater than 21 you will lose game

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 12
+let sum = firstCard + secondCard +6 // 23
 
-let sum = firstCard + secondCard
-
-console.log(sum)
+if (sum  < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}  else if (sum > 21) {
+    console.log("You're out of the game! 😭")
+}
