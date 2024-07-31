4f94dddc4473f706c259683d2ecc8c612804e620
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-31 15:48:44 +0200

    display the sum on screen by adding get element by id sum-el

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 let firstCard = 10;
-let secondCard= 11;
+let secondCard= 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-console.log(messageEl)
+let sumEl = document.getElementById("sum-el");
+
 
 function startGame() {
+    
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
