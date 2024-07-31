1471cae910321da3a7692a041a8596006aa71edc
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-31 21:17:08 +0200

    add game variables and DOM element retrieval

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
       chips: 200
   }
   if (playerName == null || playerName == "") player.name = "Guest";
+  let cards = []
+ let sum = 0
+ let hasBlackJack = false
+ let isAlive = false
+ let message = ""
+ let messageEl = document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+ let cardsEl = document.getElementById("cards-el")
+ let playerEl = document.getElementById("player-el")
+ let startGameBtn = document.getElementById("start-game-btn")
+ let newCardBtn = document.getElementById("new-card-btn")
