36158c146799c6e8fb3affd051f90e658a11cdbd
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-07-31 21:13:53 +0200

    create variable to prompt user to enter name, if no name provided it will default to guest and initializes a player object with name and starting amount of chips

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded");
+  let playerName = prompt("Please Enter your name:", 'Guest');
+  let player = {
+      name: playerName,
+      chips: 200
+  }
+  if (playerName == null || playerName == "") player.name = "Guest";
